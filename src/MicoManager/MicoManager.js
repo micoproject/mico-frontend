0426b90e69775ad5067c26ba78f41611f75c7cf4
Stefaan Ponnet <stefaan@ponnet.com>
2018-08-19 13:58:42 +0200

    removed ipfs-api from build ( for now )

--- src/MicoManager/MicoManager.js
+++ src/MicoManager/MicoManager.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./MicoManager.css";
 import Board from "react-trello";
-import ipfsAPI from "ipfs-api";
+// import ipfsAPI from "ipfs-api";
 
 class MicoManager extends Component {
   // Adds a class constructor that assigns the initial state values:
   constructor() {
     super();
-    this.ipfs = ipfsAPI("ipfsapi.swarm.city", "443", { protocol: "https" }); // leaving out the arguments will default to these values
+    // this.ipfs = ipfsAPI("ipfsapi.swarm.city", "443", { protocol: "https" }); // leaving out the arguments will default to these values
 
     this.setEventBus = this.setEventBus.bind(this);
     this.dataChanged = this.dataChanged.bind(this);
@@ -71,9 +71,9 @@ class MicoManager extends Component {
 
   dataChanged(newData) {
     console.log("new data", newData);
-    this.ipfs.add(new this.ipfs.Buffer(JSON.stringify(newData)), function(err, res) {
-      console.log('new state saved => ' + res[0].hash);
-    });
+    // this.ipfs.add(new this.ipfs.Buffer(JSON.stringify(newData)), function(err, res) {
+    //   console.log('new state saved => ' + res[0].hash);
+    // });
   }
 
   // The render method contains the JSX code which will be compiled to HTML.
