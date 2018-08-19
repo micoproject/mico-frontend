ccba77507f4b6b8b051d8c3d239d3aa9ce205214
Stefaan Ponnet <stefaan@ponnet.com>
2018-08-19 14:02:22 +0200

    added header logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import MicoManager from "./MicoManager/MicoManager";
 
@@ -15,7 +15,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Mico Manager</h1>
         </header>
           <MicoManager project={this.state.projectid} />
